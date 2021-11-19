27d17537d69b3379a9c108beb8b67fc1f35ecbac
JhonDosman <dosman.diazjhon4166@gmail.com>
2021-11-19 13:53:05 -0500

    Update login.js
    Lo basico del login

--- kibhu/src/components/web/login.js
+++ kibhu/src/components/web/login.js
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom"
+import {Form} from "react-bootstrap"
 
 
 
 const Login = () => {
     return (
         <div>
-            <h2>Login</h2>
+            <h2 className="text-center">Login</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Usuario</Form.Label>
+                <Form.Control className="" type="user" placeholder="Ingrese Usuario" />
+                </Form.Group>
+
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Contraseña</Form.Label>
+    <Form.Control type="password" placeholder="Ingrese Contraseña" />
+</Form.Group>
+<Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Recuerdame" />
+</Form.Group>
+</Form>
             <button>
             <Link to="/kibhu"> Entrar </Link>
             </button>
