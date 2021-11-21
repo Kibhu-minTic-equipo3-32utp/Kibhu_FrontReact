7f09af4a5e0511af167911eccf6f75ec2053878c
TarAldarion1437 <alejolarias542@gmail.com>
2021-11-20 23:50:13 -0500

    Precios y servicios melos caramelos

--- kibhu/src/components/web/prices.js
+++ kibhu/src/components/web/prices.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Col from 'react-bootstrap/Col';
+import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
+import Container from 'react-bootstrap/Container';
+
 
 export default class Precios extends Component {
 
@@ -9,46 +14,49 @@ export default class Precios extends Component {
         return (
             <div class="pricing text-center bloque" id="Precios">
                 <br />
-                <h1><b>Precios</b></h1>
-                    <p>
+                <h1><Badge pill bg="secondary">PRECIOS</Badge></h1>
+                    <br></br>
+                    <h3>
                         Mira nuestras tarifas y escoge la que mejor se ajuste a tu plan de
                         negocios.
-                    </p>
+                    </h3>
+                    <br></br>
+                    <Container fluid> 
                     <CardGroup className="row row-cols-1 row-cols-md-3 mb-3 text-center">
                         
                         <Col>  
                         <Card className="mb-4 rounded-3 shadow-sm">
-                                <Card.Title class="card-header py-3 bg-warning">
-                                    <h4 class="my-0 fw-normal">Plan Básico</h4>
+                                <Card.Title class="card-header py-3 bg-light">
+                                    <h4 class="my-0 fw-normal">Prueba gratis</h4>
                                 </Card.Title>
                                 <Card.Body>
                                     <Card.Subtitle class="card-title pricing-card-title">
-                                        $15<small class="text-muted fw-light">/yr</small>
+                                        $0<small class="text-muted fw-light">/yr</small>
                                     </Card.Subtitle>
                                     <Card.Text>
-                                    <ul class="list-unstyled mt-3 mb-4">
-                                        <li>
-                                            No tienes que empezar de cero tu inventario: lo actualizamos por
-                                            ti
-                                        </li>
-                                        <li>Almacenamiento en la nube. Siempre disponible para ti</li>
-                                        <li>Guía de uso</li>
-                                        <li>
-                                            Asesorías personalizadas para darle mejor uso a tu inventario
-                                        </li>
-                                    </ul>
+                                    <ListGroup variant="flush">
+                                        <ListGroupItem>
+                                            Prueba de una semana de la aplicación
+                                        </ListGroupItem>
+                                        <ListGroupItem>
+                                            Guía de uso
+                                        </ListGroupItem>
+                                        <ListGroupItem>
+                                            Guarda tu inventario con nosotros y si decides seguir se actualizará
+                                        </ListGroupItem>
+                                    </ListGroup>
                                     </Card.Text>
                                     <button 
-                                        variant="warning"
+                                        variant="secondary"
                                     >
-                                        Únete ya
+                                        Registrate aquí
                                     </button>
                                 </Card.Body>
                             </Card>
                         </Col>
                         <Col>
                             <Card className="mb-4 rounded-3 shadow-sm">
-                                <Card.Title class="card-header py-3 bg-warning">
+                                <Card.Title class="card-header py-3 bg-secondary">
                                     <h4 class="my-0 fw-normal">Plan Básico</h4>
                                 </Card.Title>
                                 <Card.Body>
@@ -56,20 +64,24 @@ export default class Precios extends Component {
                                         $15<small class="text-muted fw-light">/yr</small>
                                     </Card.Subtitle>
                                     <Card.Text>
-                                    <ul class="list-unstyled mt-3 mb-4">
-                                        <li>
+                                    <ListGroup variant="flush">
+                                        <ListGroupItem>
                                             No tienes que empezar de cero tu inventario: lo actualizamos por
                                             ti
-                                        </li>
-                                        <li>Almacenamiento en la nube. Siempre disponible para ti</li>
-                                        <li>Guía de uso</li>
-                                        <li>
+                                        </ListGroupItem>
+                                        <ListGroupItem>
+                                            Almacenamiento en la nube. Siempre disponible para ti
+                                        </ListGroupItem>
+                                        <ListGroupItem>
+                                            Guía de uso
+                                        </ListGroupItem>
+                                        <ListGroupItem>
                                             Asesorías personalizadas para darle mejor uso a tu inventario
-                                        </li>
-                                    </ul>
+                                        </ListGroupItem>
+                                    </ListGroup>
                                     </Card.Text>
                                     <button 
-                                        variant="warning"
+                                        variant="secondary"
                                     >
                                         Únete ya
                                     </button>
@@ -79,34 +91,39 @@ export default class Precios extends Component {
                         <Col>
                         <Card className="mb-4 rounded-3 shadow-sm">
                                 <Card.Title class="card-header py-3 bg-warning">
-                                    <h4 class="my-0 fw-normal">Plan Básico</h4>
+                                    <h4 class="my-0 fw-normal">Premium</h4>
                                 </Card.Title>
                                 <Card.Body>
                                     <Card.Subtitle class="card-title pricing-card-title">
-                                        $15<small class="text-muted fw-light">/yr</small>
+                                        $29<small class="text-muted fw-light">/yr</small>
                                     </Card.Subtitle>
                                     <Card.Text>
-                                    <ul class="list-unstyled mt-3 mb-4">
-                                        <li>
-                                            No tienes que empezar de cero tu inventario: lo actualizamos por
-                                            ti
-                                        </li>
-                                        <li>Almacenamiento en la nube. Siempre disponible para ti</li>
-                                        <li>Guía de uso</li>
-                                        <li>
+                                    <ListGroup variant="flush">
+                                        <ListGroupItem>
+                                            Une todas tus franquicias en un solo inventario
+                                        </ListGroupItem>
+                                        <ListGroupItem>
+                                            Almacenamiento en la nube. Siempre disponible para ti
+                                        </ListGroupItem>
+                                        <ListGroupItem>
                                             Asesorías personalizadas para darle mejor uso a tu inventario
-                                        </li>
-                                    </ul>
+                                        </ListGroupItem>
+                                        <ListGroupItem>
+                                            Reporte constante sobre el desempeño de tu inventario
+                                            con la mejor información disponible para el crecimiento de tu empresa
+                                        </ListGroupItem>
+                                    </ListGroup>
                                     </Card.Text>
                                     <button
-                                        variant="warning"
+                                        variant="secondary"
                                     >
-                                        Únete ya
+                                        ¿Te interesa?
                                     </button>
                                 </Card.Body>
                             </Card>
                         </Col>
                     </CardGroup>
+                    </Container>
                     </div>
                     )
     }
--- kibhu/src/components/web/services.js
+++ kibhu/src/components/web/services.js
@@ -1,14 +1,63 @@
 import React, {Component} from 'react';
 import './Services.css';
-
+import Badge from 'react-bootstrap/Badge';
+import Accordion from 'react-bootstrap/Accordion';
+import AccordionItem from 'react-bootstrap/esm/AccordionItem';
+import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
+import AccordionBody from 'react-bootstrap/esm/AccordionBody';
+import Container from 'react-bootstrap/Container';
 
 export default class Servicios extends Component {
 
     render () {
         return (
             <div classname="Servicios">
-                <h1>  Servicios de Kibhu</h1>
-                <h2> Nuestros servicios son:  </h2>
+                <h1 class="text-center">
+                    <Badge pill bg="secondary">SERVICIOS</Badge> </h1>
+                    <br></br>
+                    <br></br>
+                    <h3 class="text-center"> ¿Qué puedes hacer con Kibhu?</h3>
+                    <br></br>
+                    <br></br>
+                    <Container fluid> 
+                    <Accordion defaultActiveKey="0" flush>
+                        <AccordionItem eventKey="0"> 
+                            <AccordionHeader>
+                                <h2>Guarda tu inventario</h2>
+                            </AccordionHeader>
+                            <AccordionBody>
+                                <h4><strong>Guarda los products que vendes, en la nube.</strong> Con nuestra app
+                            puedes hacerlo. Tu inventario no sólo estará seguro, sino que será de fácil acceso 
+                            siempre que necesites revisarlo</h4>
+                            </AccordionBody>
+                        </AccordionItem>
+                        <AccordionItem eventKey="1"> 
+                            <AccordionHeader><h2>Actualiza tus productos</h2></AccordionHeader>
+                            <AccordionBody>
+                                <h4><strong>Al realizar una venta o compra de un producto.</strong> Podrás mirar en 
+                            tiempo real la cantidad que queda de ese item, de forma rápida y eficaz tu inventario 
+                            se actualiza para mantenerte al tanto de tus necesidades.</h4>
+                            </AccordionBody>
+                        </AccordionItem>
+                        <AccordionItem eventKey="2"> 
+                            <AccordionHeader><h2>Mejora tu empresa</h2></AccordionHeader>
+                            <AccordionBody>
+                                <h4><strong>Mira el desempeño de tus productos:</strong> los más vendidos, 
+                            más demandados, dificiles de conseguir, y mucha más información para que
+                            siempre puedas tomar la mejor decisión.</h4>
+                            </AccordionBody>
+                        </AccordionItem>
+                        <AccordionItem eventKey="3"> 
+                            <AccordionHeader><h2>Ve más allá</h2></AccordionHeader>
+                            <AccordionBody>
+                                <h4><strong>No te conformes</strong> a organizar tu inventario de productos,
+                            con Kibhu puedes armar tu propia base de datos con información de clientes,
+                            proveedores, y empleados. Así puedes detectar clientes destacados, anotar 
+                            los proveedores más necesarios y destacar tus empleados.</h4>
+                            </AccordionBody>
+                        </AccordionItem>
+                    </Accordion>
+                    </Container>
             </div>
         )
     }
