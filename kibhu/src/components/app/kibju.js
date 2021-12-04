ae3891d3dbf5fe6fc0082ca10aaa0efe03d8295f
Acosticahub <acostabuitrago0123@gmail.com>
2021-12-04 17:02:26 -0500

    Carticas

--- kibhu/src/components/app/kibju.js
+++ kibhu/src/components/app/kibju.js
@@ -5,6 +5,7 @@ import { SuplierProvider } from "../../controllers/Suplier.controller";
 import logo from "../../assets/logo pequeño kibhu new.png";
 
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/esm/Container";
 
 function Kibhu() {
   const { handleLogout } = useContext(AuthContext);
@@ -37,12 +38,91 @@ function Kibhu() {
       <SuplierProvider>
         <Outlet />
       </SuplierProvider>
-      <Card>
-        <Card.Body>
-          <Card.Title>Inventario</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">4</Card.Subtitle>
-        </Card.Body>
-      </Card>
+      <br></br>
+      <br></br>
+      <Container>
+        <div class="row carticas">
+          <div class="col-xl-3 col-md-6 mb-4">
+            <a href="/productos">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">
+                        Inventario
+                      </div>
+                      <p class="bold">4</p>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-warning"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-xl-3 col-md-6 mb-4">
+            <a href="/usuarios">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">
+                        Usuarios
+                      </div>
+                      <p class="bold">4</p>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-user fa-2x text-warning"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-xl-3 col-md-6 mb-4">
+            <a href="/clientes">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">
+                        Clientes
+                      </div>
+                      <p class="bold">4</p>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-clipboard-list fa-2x text-warning"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-xl-3 col-md-6 mb-4">
+            <a href="/proveedores">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">
+                        Proveedores
+                      </div>
+                      <p class="bold">4</p>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-comments fa-2x text-warning"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
