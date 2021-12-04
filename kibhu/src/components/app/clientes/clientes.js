a1eca3cd2c090a911eebf7cf5744c4eaca64c056
TarAldarion1437 <alejolarias542@gmail.com>
2021-12-04 13:03:40 -0500

    Update clientes.js

--- kibhu/src/components/app/clientes/clientes.js
+++ kibhu/src/components/app/clientes/clientes.js
@@ -1,35 +1,34 @@
 //Clientes y su catalogo será trabajado con hooks
 import Badge from 'react-bootstrap/Badge';
 
-
+import React, { useContext} from 'react';
 import ClientTable from './ClientTable';
-import {useState} from 'react';
 import AddClientForm from './AddClientForm';
 import EditClientForm from './EditClientForm';
+import ClientContext from '../../../controllers/Client.controller';
 
 
+const Clientes = () => {
+    
+    const { clients } = useContext(ClientContext);
 
-export default function Clientes() {
-    const clientData = [
-        { firstname: "carlos", lastname: "Ceballos", mail:"carlos@gmail.com", contact:"931293912", identification:"01", typeid:"CC"},
-        { firstname: "daniel", lastname: "Ceballos", mail:"carlos@gmail.com", contact:"931293912", identification:"02", typeid:"CC"},
-        { firstname: "andrés", lastname: "Ceballos", mail:"carlos@gmail.com", contact:"931293912", identification:"03", typeid:"CC"}
-    ]
-
-    // para ver los clientes
-
-    const[clients, setClients] =useState(clientData)
+    
 
     //para añadir cliente
 
-    const addClient =  (client) => {
+    /*const addClient =  (client) => {
         setClients([
             ...clients,
             client
         ])
     }
+    <ClientTable 
+                    clients={clients} 
+                    deleteClient={deleteClient} 
+                    editRow={editRow}
+                    /> 
 
-    // para Eliminar cliente
+    //para Eliminar cliente
 
     const deleteClient = (identification) => {
 
@@ -60,7 +59,7 @@ export default function Clientes() {
     const updateClient = (identification, updatedClient) => {
         setEditing(false);
         setClients(clients.map(client => (client.identification === identification ? updatedClient : client)))
-    }
+    }*/
 
 
     return(
@@ -70,17 +69,20 @@ export default function Clientes() {
                     <h1><Badge pill bg="secondary">CLIENTES DE KIBHU</Badge></h1>
                     <br></br>
                     <br></br>
-                    <ClientTable 
-                    clients={clients} 
-                    deleteClient={deleteClient} 
-                    editRow={editRow}
-                    />
+                    <div>
+                    {clients.map((e) => (
+                        <ClientTable
+                        key={e.identification}
+                        objClient={e}
+                        />
+                    ))}
+                    </div>
                 </div>
                 <br></br>
                 <br></br>
                 <br></br>
                 <br></br>
-                <div className="flex-large">
+                {/*  <div className="flex-large">
                     {
                         editing ? (
                             <div>
@@ -102,9 +104,10 @@ export default function Clientes() {
                         )
                     }
                     
-                </div>
+                </div> */}
             </div>
         </div>
     );    
-}
+};
 
+export default Clientes;
