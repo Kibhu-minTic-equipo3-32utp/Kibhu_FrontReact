841a474c98a37e2dd25b7f3952c091ba021d38da
TarAldarion1437 <alejolarias542@gmail.com>
2021-12-05 17:03:16 -0500

    vamos

--- kibhu/src/routes/AuthRouter.js
+++ kibhu/src/routes/AuthRouter.js
@@ -3,14 +3,14 @@ import {Route, Routes} from 'react-router-dom';
 import Kibhu from '../components/app/kibju';
 import Clientes from '../components/app/clientes/clientes';
 import Supliers from '../components/app/proveedores/proveedores';
-
-
+import Usuarios from '../components/app/usuarios/usuarios';
 const Authrouter = () => {
     return(
         <Routes>
             <Route path="/" element={< Kibhu />}>
                 <Route path="clientes" element={<Clientes/>}/>
                 <Route path="proveedores" element={<Supliers/>}/>
+                <Route path="usuarios" element={<Usuarios/>} />
             </Route>
         </Routes>
     )
