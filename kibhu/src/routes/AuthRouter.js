a73214b3db6a5dba2d43d2555edf18d32ccd1d88
TarAldarion1437 <alejolarias542@gmail.com>
2021-12-05 17:19:55 -0500

    ruta productos

--- kibhu/src/routes/AuthRouter.js
+++ kibhu/src/routes/AuthRouter.js
@@ -4,6 +4,8 @@ import Kibhu from '../components/app/kibju';
 import Clientes from '../components/app/clientes/clientes';
 import Supliers from '../components/app/proveedores/proveedores';
 import Usuarios from '../components/app/usuarios/usuarios';
+import Productos from '../components/app/productos/productos';
+
 const Authrouter = () => {
     return(
         <Routes>
@@ -11,6 +13,7 @@ const Authrouter = () => {
                 <Route path="clientes" element={<Clientes/>}/>
                 <Route path="proveedores" element={<Supliers/>}/>
                 <Route path="usuarios" element={<Usuarios/>} />
+                <Route path="productos" element={<Productos/>} />
             </Route>
         </Routes>
     )
